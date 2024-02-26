45b17235450da9cae7db5acb1eaecc301eaf5d7c
shoma <shoma.albakov69@gmail.com>
2024-02-26 08:14:17 +0300

    Update dependencies and adjust code formatting

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,7 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-import eslint from 'vite-plugin-eslint';
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-      react(),
-    eslint({ exclude: ['**/virtual:/**', '**/node_modules/**'] }),
-  ],
+  plugins: [react()],
 })
